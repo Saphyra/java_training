ad24f4b19c6d83baf3a574e14a11259c92ae826c
saphyra <bondabear@gmail.com>
2020-11-27 09:28:41 -0800

    Generikus osztaly

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
+        List<String> lista = new ArrayList<String>();
+        Map<String, String> map = new HashMap<>();
+        lista.add("asd");
+        lista.add("aedfea");
+        lista.add("wsfgrweg");
+
+        lista.get(0);
+
+        for (String o : lista) {
+        }
+
+        Doboz<String> doboz = Doboz.letrehoz("érték");
+        String ertek = doboz.getErtek();
+    }
+
+    private static void printLista(List lista) {
 
     }
 }
