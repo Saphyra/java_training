46d21b47cf601e76e76dcb9c5382f2bf637593aa
saphyra <bondabear@gmail.com>
2020-12-10 08:27:22 -0800

    RestController

---
+++ src/main/java/com/github/saphyra/response/UsernameResponse.java
@@ -0,0 +1,10 @@
+package com.github.saphyra.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class UsernameResponse {
+    private String username;
+}
